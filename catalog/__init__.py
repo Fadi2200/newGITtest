4535b88b09e0dd98225f40992e695ffd3241c4c5
fadi2200 <fadialmachot@hotmail.com>
2020-11-25 20:28:09 +0100

    First commit 9

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -1,6 +1,34 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
-from models import db, Contact
-from forms import ContactForm, myChoices
+#from models import db, Contact
+#from forms import ContactForm, myChoices
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired, Email, Length
+
+
+myChoices = {"Bosch" : "Bosch", 
+             "Strasse":"Strasse",
+             "Not Found":"Not Found",
+             "Schelter":"Schelter",
+             "In active state":"In active state",
+             "Levcon":"Levcon"}
+            
+            
+
+
+class ContactForm(FlaskForm):
+    barcode = StringField('Barcode', validators=[Length(min=5, max=80, message='You cannot have more than 200 characters')])
+    kennzeichen = StringField('Kennzeichen', validators=[DataRequired(), Length(min=5, max=80, message='You cannot have more than 80 characters')])
+    herkunft =SelectField(u'Herkunft', choices =myChoices, validators=[Length(min=-1, max=200, message='You cannot have more than 100 characters')])
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/book'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 # Flask
 app = Flask(__name__)
@@ -105,6 +133,24 @@ def contacts_delete():
 
     return redirect(url_for('contacts'))
 
+
+
+class Contact(db.Model):
+
+
+    __tablename__ = 'contacts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    #name = db.Column(db.String(80), nullable=True)
+    #surname = db.Column(db.String(100), nullable=True)
+    #email = db.Column(db.String(200), nullable=True, unique=False)
+    #phone = db.Column(db.String(20), nullable=True, unique=False)
+    kennzeichen= db.Column(db.String(200), nullable=False)
+    herkunft= db.Column(db.String(200), nullable=False)
+    barcode= db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Contacts %r>' % self.barcode
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
